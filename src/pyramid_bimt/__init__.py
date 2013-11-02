30dbea0c4a2029be5edab90c31bdc9bd4465c2f1
Jaka Hudoklin <jakahudoklin@gmail.com>
2013-11-02 19:45:45 +0100

    init: put common configuration to configuration function, so we can easily use it in functional tests

--- src/pyramid_bimt/__init__.py
+++ src/pyramid_bimt/__init__.py
@@ -10,32 +10,18 @@ from pyramid_bimt.acl import groupfinder
 from pyramid_bimt.hooks import get_authenticated_user
 
 
-def includeme(config):
-    """Allow developers to use ``config.include('pyramid_bimt')``."""
-
-    # Setup the DB session and such
-    config.include('pyramid_basemodel')
+def configure(config, settings={}):
 
     # Include pyramid layout
     config.include('pyramid_layout')
 
     # Add helpful properties to the request object
-    settings = config.registry.settings
     config.set_request_property(
         get_authenticated_user, 'user', reify=True)
 
-    # Setup authentication and authorization policies.
+    # Setup session factory
     session_factory = UnencryptedCookieSessionFactoryConfig(
         settings.get('session.secret', 'secret'))
-    authentication_policy = AuthTktAuthenticationPolicy(
-        secret=settings.get('authtkt.secret', 'secret'),
-        hashalg='sha512',
-        callback=groupfinder,
-    )
-    authorization_policy = ACLAuthorizationPolicy()
-
-    config.set_authentication_policy(authentication_policy)
-    config.set_authorization_policy(authorization_policy)
     config.set_session_factory(session_factory)
 
     # configure routes
@@ -60,3 +46,26 @@ def includeme(config):
 
     # Run a venusian scan to pick up the declarative configuration.
     config.scan('pyramid_bimt', ignore='pyramid_simpleauth.tests')
+
+
+def includeme(config):
+    """Allow developers to use ``config.include('pyramid_bimt')``."""
+
+    # Get settings
+    settings = config.registry.settings
+
+    # Setup the DB session and such
+    config.include('pyramid_basemodel')
+
+    # Setup authentication and authorization policies.
+    authentication_policy = AuthTktAuthenticationPolicy(
+        secret=settings.get('authtkt.secret', 'secret'),
+        hashalg='sha512',
+        callback=groupfinder,
+    )
+    authorization_policy = ACLAuthorizationPolicy()
+
+    config.set_authorization_policy(authorization_policy)
+    config.set_authentication_policy(authentication_policy)
+
+    configure(config, settings)
