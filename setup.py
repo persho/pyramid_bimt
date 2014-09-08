a81ced762187a6583c34ab2c0e877fe1b81e651d
Vanč Levstik <vanc.levstik@gmail.com>
2014-09-08 15:38:43 +0200

    Back to development: 0.16.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = \
 
 setup(
     name='pyramid_bimt',
-    version='0.16',
+    version='0.16.1.dev0',
     description='Base package for BIMT apps.',
     long_description=long_description,
     classifiers=[
