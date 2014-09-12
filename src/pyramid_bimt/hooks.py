dbfd9732bac1248425a6a55441bcd8831f6b3fa0
Vanč Levstik <vanc.levstik@gmail.com>
2014-09-12 14:26:50 +0200

    Replace now deprecated unauthenticated_userid method

--- src/pyramid_bimt/hooks.py
+++ src/pyramid_bimt/hooks.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """Various helpers."""
-
-from pyramid.security import unauthenticated_userid
 from pyramid_bimt.models import User
 
 
 def get_authenticated_user(request):
     """Get the authenticated user for this ``request``, if there is one."""
-    email = unauthenticated_userid(request)
-    if email:
-        return User.by_email(email)
+    if request:
+        email = request.unauthenticated_userid
+        if email:
+            return User.by_email(email)
