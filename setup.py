ab4c4282f3be83afdae22c8697f3e699b9aa8e41
Nejc Zupan <nejc.zupan@gmail.com>
2015-06-19 15:12:26 +0200

    Back to development: 0.42

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = \
 
 setup(
     name='pyramid_bimt',
-    version='0.41',
+    version='0.42.dev0',
     description='Base package for BIMT apps.',
     long_description=long_description,
     classifiers=[
